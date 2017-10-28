9d7ffee0bafea32371de21be500aeff250b02a9e
CarlosMena <carlosmena007@gmail.com>
2017-10-28 01:15:04 -0500

    Tarea 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,23 @@ export class AppComponent {
   title = 'app';
   name = 'Carlos Mena';
   colores = ['negro', 'rojo', 'blanco'];
+  
+  stats = false;
+  message = 'Show';
 
   addNewColor(color) {
     this.colores.push(color.value);
     color.value = "";
     return false;
+  }
+
+  showArrayColors() {
+    this.stats = !this.stats;
+    if(this.message == 'Hide') {
+      this.message = 'Show';
+    }
+    else {
+      this.message = 'Hide';
+    }
   }
 }
